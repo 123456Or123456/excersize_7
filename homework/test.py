41780c253eff632d2baa87543f3c334fa8782851
Or David <orda7697@gmail.com>
2025-07-31 18:25:41 +0300

    ani virues

--- homework/test.py
+++ homework/test.py
@@ -1,2 +1,12 @@
-print("hello world")
+import os #גישה לתיקיות ולקבצים
+import requests#בקשות לרשת api (כמן פוסט)
 
+scan_url= 'https://www.virustotal.com/vtapi/v2/file/scan' 
+curl_request_url = 'https://www.virustotal.com/vtapi/v2/url/report?apikey=<apikey>&resource=<resource>'
+my_api ='3f36b4c89659eaaa70f13a296ea4c2eaa0cbb532c2f6f9b305f57033ad7b66f7'
+def scan_file(file_path):
+    print("scaning file", file_path)
+
+
+def scan_folder(older_path):
+    pass
