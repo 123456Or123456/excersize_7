6fd1421f217b680cae5e1c912025df36c5b131fd
Or David <orda7697@gmail.com>
2025-08-02 18:05:28 +0300

    day1

--- homework/systask1.py
+++ homework/systask1.py
@@ -1,16 +1,25 @@
+import sys 
+import re
 def word_frequency_counter(file_name,top_num):
-    with open(file_name, 'r') as file:
+    with open(file_name, 'r',encoding='utf-8') as file:
         text=file.read()
         words=text.split()
+#words = re.findall(r'\b\w+\b', text.lower())
 
-        word_frequenc={}
+        word_frequency={}
         for word in words:
-            if word in word_frequenc:
-                word_frequenc[word]+=1
+            if word in word_frequency:
+                word_frequency[word]+=1
             else:
-                word_frequenc[word]=1
-        print(word_frequenc)
+                word_frequency[word]=1
+        print(word_frequency)
+        sorted_words = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
+
+# הדפסת top_num מילים
+#for word, freq in sorted_words[:top_num]:
+#    print(f"{word}: {freq}")
+
         
-word_frequency_counter("notes.txt",3)
+word_frequency_counter("c:/inetpub/wwwroot/CSharp/OR_JS_16.12.2024/excersize_7/homework/notes.txt", 1)
 
                 
--- homework/test.py
+++ homework/test.py
@@ -4,6 +4,7 @@ import requests#בקשות לרשת api (כמן פוסט)
 scan_url= 'https://www.virustotal.com/vtapi/v2/file/scan' 
 curl_request_url = 'https://www.virustotal.com/vtapi/v2/url/report?apikey=<apikey>&resource=<resource>'
 my_api ='3f36b4c89659eaaa70f13a296ea4c2eaa0cbb532c2f6f9b305f57033ad7b66f7'
+
 def scan_file(file_path):
     print("scaning file", file_path)
 
